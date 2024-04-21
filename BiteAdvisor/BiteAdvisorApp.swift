4d6d9905ea8c7c0fba315d9f9d23ad32ef812c09
Timea Bartha <timea.bartha.93@gmail.com>
2024-04-21 15:41:59 +0200

    Initial Commit

---
+++ BiteAdvisor/BiteAdvisorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiteAdvisorApp.swift
+//  BiteAdvisor
+//
+//  Created by Timea Bartha on 21/4/24.
+//
+
+import SwiftUI
+
+@main
+struct BiteAdvisorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
