4345d8868c620320b8e6f75913e082b794c46f80
Timea Bartha <timea.bartha.93@gmail.com>
2024-04-23 15:49:25 +0200

    Removed rotation- effect code
    The initial plan was to swipe left-right for the cards to move but I used buttons in the end as this was an assignment, and buttons where necessary

--- BiteAdvisor/View/CardView.swift
+++ BiteAdvisor/View/CardView.swift
@@ -11,71 +11,33 @@ struct CardView: View {
     @ObservedObject var networkManager = NetworkManager()
     @ObservedObject var locationviewModel = locationViewModel()
     
-    @State private var offset = CGSize.zero
     var restaurant: Business
     
     var body: some View {
         VStack {
             ZStack {
-                    RoundedRectangle(cornerRadius: 25.0, style: .continuous)
-                        .fill(.gray)
-                    GeometryReader { geometry in
-                        VStack {
-                            AsyncImage(url: URL(string: restaurant.imageURL)) { image in image.resizable() } placeholder: { Color.red }
-                                .padding(10)
-                                .clipShape(RoundedRectangle(cornerRadius: 25))
-                                .scaledToFit()
-                                .frame(width: geometry.size.width, height: geometry.size.height * 0.65)
-                                
-                                
-                
-                            Text(restaurant.name)
-                                .font(.title)
-                                .bold()
-                            Text(" Rating: \(restaurant.rating, specifier: "%.1f")")
-                                .font(.headline)
-                            
-                        }
+                RoundedRectangle(cornerRadius: 25.0, style: .continuous)
+                    .fill(.green.opacity(0.3))
+                GeometryReader { geometry in
+                    VStack {
+                        AsyncImage(url: URL(string: restaurant.imageURL)) { image in image.resizable() } placeholder: { Color.red }
+                            .padding(10)
+                            .clipShape(RoundedRectangle(cornerRadius: 25))
+                            .scaledToFit()
+                            .frame(width: geometry.size.width, height: geometry.size.height * 0.65)
+                        Text(restaurant.name)
+                            .font(.title)
+                            .bold()
+                            .foregroundColor(.primary)
+                        Text(" Rating: \(restaurant.rating, specifier: "%.1f")")
+                            .font(.headline)
+                            .foregroundColor(.primary)
                     }
-                }.padding(30)
-                    .rotationEffect(.degrees(Double(offset.width / 5)))
-                    .offset(x: offset.width * 5, y: 0) // move left and right but never up and down
-                    .gesture(
-                        DragGesture()
-                            .onChanged { gesture in
-                                offset = gesture.translation
-                                
-                            }
-                            .onEnded { _ in
-                                if abs(offset.width) > 100 { // abs = absolute number of that value
-                                    if offset.width > 0 {
-                                    }
-                                    
-                                } else {
-                                    offset = .zero
-                                }
-                            }
-                    )
-                .animation(.spring(), value: offset)
-           
+                }
+            }.padding(30)
+                
         }
         .padding(10)
-        /* .task {
-                    do {
-                        restaurant = try await self.networkManager.fetchData(latitude: locationviewModel.userLatitude, longitude: locationviewModel.userLongitude)
-                    } catch YelpError.invalidURL {
-                        print("invalud URL")
-                    } catch YelpError.invalidData {
-                        print("invalid data")
-                    } catch YelpError.invalidResponse {
-                        print("invalid response")
-                    } catch {
-                        print("unexpected error")
-                    }
-                } */
-        }
+    }
 }
 
-//#Preview {
-//    CardView()
-//}
